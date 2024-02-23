997b2a45b8d64a117d0c07ee61eb4374eddcfd81
Dhirendra Gohil <dhigohil@publicisgroupe.net>
2024-02-23 11:23:07 +0530

    webhook try

--- src/main/java/HelloWorldJenkins.java
+++ src/main/java/HelloWorldJenkins.java
@@ -2,7 +2,11 @@ public class HelloWorldJenkins {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world from Jenkins learning!!");
+        System.out.println("**********************");
+        System.out.println("########## Hello world from Jenkins learning!! ##########");
+        System.out.println("**********************");
+        System.out.println("############# Trying Webhook to trigger build!! ################");
+        System.out.println("**********************");
 
     }
 
